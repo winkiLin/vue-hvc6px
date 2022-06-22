c11325b5d485d15ed297c2e52f8895bf3080fd3d
winkiLin <1614958628@qq.com>
2022-06-22 17:58:00 +0800

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,15 @@ export default {
 </script>
 
 <style>
+body {
+  background: black;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #d7d7d7;
   margin-top: 60px;
 }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="login">这是测试页面</div>
+  <div id="login">页面</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: 'Login',
   components: {},
 };
 </script>
